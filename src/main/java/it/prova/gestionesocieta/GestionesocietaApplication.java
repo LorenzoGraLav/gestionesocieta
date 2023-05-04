9c564fd7643da978d91c6169369c8485556570b5
Lorenzo <lorenzogra2022@libero.it>
2023-05-04 15:01:17 +0200

    caricamento progetto

---
+++ src/main/java/it/prova/gestionesocieta/GestionesocietaApplication.java
@@ -0,0 +1,20 @@
+package it.prova.gestionesocieta;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionesocietaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionesocietaApplication.class, args);
+		System.out.println("Benvenuto in Spring");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
---
+++ src/main/java/it/prova/gestionesocieta/repository/DipendenteRepository.java
@@ -0,0 +1,5 @@
+package it.prova.gestionesocieta.repository;
+
+public interface DipendenteRepository {
+
+}
